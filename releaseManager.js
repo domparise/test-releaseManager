1746ab9a3075a2cc801930bb55a08c0a8cdf61ed
domparise@gmail.com <domparise@gmail.com>
2015-09-11 18:38:56 -0400

    gonna push to server and test

---
+++ releaseManager.js
@@ -0,0 +1,36 @@
+// https://developer.github.com/guides/managing-deploy-keys/#deploy-keys
+
+var manager = require('express')()
+var child_process = require('child_process')
+
+manager.use(require('body-parser').json())
+
+var version = '2.2.0'
+var lastUpdate = new Date()
+var author = ''
+
+var server = child_process.spawn('node',['src/app'])
+
+manager.post('/git/release', function (req, res) {
+    if (req.header('X-Github-Event') && req.header('X-Github-Event') === 'release') {
+        console.log('release '+version)
+        server.kill()
+        child_process.exec('git pull', function (err) {
+            if (err) console.log(err)
+            server = child_process.spawn('node',['src/app'])
+            lastUpdate = new Date()
+            version = req.body.release.tag_name
+            author = req.body.release.author.login
+            console.log('update complete')
+        })
+    }
+    res.send(200)
+})
+
+manager.get('/version', function (res,res) {
+    res.send({version:version,lastUpdate:lastUpdate,author:author})
+})
+
+manager.listen(4005, function () {
+    console.log('manager online port 4005')
+})
---
+++ src/app.js
@@ -0,0 +1,20 @@
+var app = require('express')();
+
+app.get('/', function (req, res) {
+    res.send('yolo')
+})
+
+app.get('/test', function (req, res) {
+    res.send('swag')
+})
+
+var httpServer = require('http').createServer(app)
+
+httpServer.listen(3005)
+
+if (process.env.NODE_ENV !== 'dev') {
+    process.on('uncaughtException', function (err) {
+        console.dir(err)
+        console.trace(err)
+    })
+}
